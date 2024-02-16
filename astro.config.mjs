5d19d9a066cdfef09f871584bfb4e7cdf4ef75f9
Alec Jensen <alecbradyjensen@gmail.com>
2024-02-16 16:39:31 -0600

    change to node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-import cloudflare from "@astrojs/cloudflare";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
-  vite: {
-    build: {
-        minify: false,
-    },
-  }
+  adapter: node({
+    mode: "standalone"
+  })
 });
